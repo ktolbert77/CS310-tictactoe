9656c2815b45e5e5c39adfe2be163c504dce20b3
Kyle Tolbert <atolbert1@stu.jsu.edu>
2017-09-25 23:14:08 -0500

    Completed GUI implementaion

--- src/main/java/edu/jsu/mcis/TicTacToe.java
+++ src/main/java/edu/jsu/mcis/TicTacToe.java
@@ -6,29 +6,29 @@ import javax.swing.*;
 
 public class TicTacToe {
 
-    private static final int DEFAULT_WIDTH = 3;
-    public static void main(String[] args) {
+    private static final int DEFAULT_WIDTH = 3;
+    public static void main(String[] args) {
 
-        /* Set initial size of game board (default is 3x3) */
-        int width = DEFAULT_WIDTH;
+        /* Set initial size of game board (default is 3x3) */
+        int width = DEFAULT_WIDTH;
 
-        /* If a different size is provided as a command-line argument, use it instead */
-        if(args.length >= 1) {
-            try {
-                width = Integer.parseInt(args[0]);
-            }
-            catch(NumberFormatException e) {}
-        }
+        /* If a different size is provided as a command-line argument, use it instead */
+        if(args.length >= 1) {
+            try {
+                width = Integer.parseInt(args[0]);
+            }
+            catch(NumberFormatException e) {}
+        }
 
-        /* Create MVC Objects */
-        TicTacToeModel model = new TicTacToeModel(width);
-        TicTacToeView view = new TicTacToeView(model);
-        
-        /* Initialize GUI */
-        JFrame win = new JFrame("Tic-Tac-Toe");
-        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        win.add(view);
-        win.pack();
-        win.setVisible(true);
-    }
+        /* Create MVC Objects */
+        TicTacToeModel model = new TicTacToeModel(width);
+        TicTacToeView view = new TicTacToeView(model);
+        
+        /* Initialize GUI */
+        JFrame win = new JFrame("Tic-Tac-Toe");
+        win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        win.add(view);
+        win.pack();
+        win.setVisible(true);
+    }
 }
--- src/main/java/edu/jsu/mcis/TicTacToeView.java
+++ src/main/java/edu/jsu/mcis/TicTacToeView.java
@@ -5,88 +5,98 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class TicTacToeView extends JPanel implements ActionListener {
-    
-    TicTacToeModel model;
+    
+    TicTacToeModel model;
 
-    private JButton[][] squares;
-    private JPanel squaresPanel;
-    private JLabel resultLabel;
+    private JButton[][] squares;
+    private JPanel squaresPanel;
+    private JLabel resultLabel;
 
-    public TicTacToeView(TicTacToeModel model) {
+    public TicTacToeView(TicTacToeModel model) {
 
-        this.model = model;
+        this.model = model;
 
-        int width = model.getWidth();
+        int width = model.getWidth();
 
-        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-        squares = new JButton[width][width];
-        squaresPanel = new JPanel(new GridLayout(width,width));
-        resultLabel = new JLabel();
-        resultLabel.setName("ResultLabel");
-        
-        /* Loop through all rows and columns of the grid and: create a new
-           JButton and assign it to the current grid element, add "this"
-           (the View object) as the ActionListener, set the JButton's name
-           to "SquareXY" (where X is the row and Y is the column), set the
-           preferred size to 64x64 pixels, and add the new square to the
-           "squaresPanel" JPanel created earlier. */
-        
-        /* INSERT CODE HERE */
-        
-        add(squaresPanel);
-        add(resultLabel);
-        
-        resultLabel.setText("Welcome to Tic-Tac-Toe!");
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        squares = new JButton[width][width];
+        squaresPanel = new JPanel(new GridLayout(width,width));
+        resultLabel = new JLabel();
+        resultLabel.setName("ResultLabel");
+        
+        
+        /* INSERT CODE HERE */
+		   for (int i = 0; i < width; i++) {
+            for (int j = 0; j < width; j++) {
+                squares[i][j] = new JButton();
+                squares[i][j].setName("Square"+i+j);
+                squares[i][j].addActionListener(this);
+                squares[i][j].setPreferredSize(new Dimension(64, 64));
+                squaresPanel.add(squares[i][j]);
+            }
+        }
+        
+        add(squaresPanel);
+        add(resultLabel);
+        
+        resultLabel.setText("Welcome to Tic-Tac-Toe!");
 
-    }
+    }
 
-    @Override
-    public void actionPerformed(ActionEvent event) {
-        
-        String name = ((JButton) event.getSource()).getName();
-        
-        /* Extract the ROW and COL from the string name (remember, the squares
-           should all be named "SquareXY", so the ROW should be extracted from
-           position 6 of this string, and the COL from position 7.  Remember to
-           convert both to Integers!) */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        
+        String name = ((JButton) event.getSource()).getName();
+     
 
-        /* INSERT CODE HERE */
-           
-        /* Call makeMark() to place the mark in the Model */
-        
-        /* INSERT CODE HERE */
+        /* INSERT CODE HERE */
+		int r = Character.getNumericValue(name.charAt(6));
+        int c = Character.getNumericValue(name.charAt(7));
+           
+        /* Call makeMark() to place the mark in the Model */
+        
+        /* INSERT CODE HERE */
+		model.makeMark(r, c);
 
-        /* Update the squares of the View using the "updateSquares()" method
-           (see below); this is the equivalent of re-printing the grid to the
-           console in Part One. */
+        
 
-        updateSquares();
+        updateSquares();
 
-        /* Clear the "showResult" label at the bottom of the View */
-        
-        showResult(" ");
-        
-        /* If the game is over (that is, if the "getResult()" method returns
-           a result other than Result.NONE), loop through all the JButtons in
-           the View and disable them to prevent further entries.  (Hint: the
-           JButton class provides a "setEnabled()" method for this.) */
-           
-        /* INSERT CODE HERE */
-        
-    }
-        
-    public void updateSquares() {
+        /* Clear the "showResult" label at the bottom of the View */
+        
+        showResult(" ");
+        
+  
+           
+        /* INSERT CODE HERE */
+		if(model.isGameover()){
+			showResult(model.getResult().toString());
+			for(int i=0; i < model.getWidth(); i++){
+				for(int j=0; j < model.getWidth(); j++){
+					squares[i][j].setEnabled(false);
+				}
+			}
+			
+		}
+				
+        
+    }
+        
+    public void updateSquares() {
 
-        /* Loop through all of the rows and columns in the JButton grid and
-           reset the button text to match the corresponding marks as returned
-           by "getMark()" (to update the View to match the Model) */
-           
-        /* INSERT CODE HERE */
+        /* INSERT CODE HERE */
+		for( int i = 0; i < model.getWidth(); i++){
+			for(int j = 0; j < model.getWidth(); j++){
+				squares[i][j].setText(model.getMark(i , j).toString());
+			}
+		}
+			
 
-    }
-        
-    public void showResult(String message) {
-        resultLabel.setText(message);
-    }
+    }
+        
+    public void showResult(String message) {
+        resultLabel.setText(message);
+    }
 
 }
+
